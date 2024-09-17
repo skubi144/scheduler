2857c4940bcb0f6986a56c6b8ee8b790ba350270
Milosz Skubala <miloszskubala@gmail.com>
2024-09-17 21:26:16 +0200

    add electron app

--- apps/view/src/index.ts
+++ apps/view/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/view';
+import {app, BrowserWindow} from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
--- apps/view/src/lib/view.spec.ts
+++
@@ -1,7 +0,0 @@
-import {view} from './view';
-
-describe('view', () => {
-  it('should work', () => {
-    expect(view()).toEqual('view');
-  });
-});
