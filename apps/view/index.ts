641d84fbfce4f93dd27fecb751e521d7fec6bc04
Milosz Skubala <miloszskubala@gmail.com>
2024-09-18 18:18:49 +0200

    run electron app

--- apps/view/index.ts
+++ apps/view/index.ts
@@ -1,10 +1,12 @@
 import {app, BrowserWindow} from 'electron'
+import {PATH12} from "./src/components/ext";
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
+  console.log(PATH12)
 
   win.loadFile('index.html')
 }
---
+++ apps/view/src/components/ext.ts
@@ -0,0 +1,1 @@
+export const PATH12 = 'PATH';
